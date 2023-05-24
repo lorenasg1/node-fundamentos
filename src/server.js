cdcaeb7e25bd5f871874e0470949872dedfaf387
lorenasg1 <lorenasg1@gmail.com>
2023-05-23 21:15:25 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import http from 'node:http'
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
+  return res.end('hello ignite')
+})
+
+server.listen(3333)
